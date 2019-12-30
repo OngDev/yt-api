397c8bbeaba68c8c1b4305ded5d126f3320d01c0
MiloNguyen95 <milonguyen95@outlook.com>
2019-12-30 10:43:03 +0700

    Basic video fetch

--- src/tasks/video.background.js
+++ src/tasks/video.background.js
@@ -1,6 +1,23 @@
 import cron from 'node-cron';
+import dotenv from 'dotenv';
+
+import YoutubeApi from '../configs/yt.config';
+
+dotenv.config();
 
 const YoutubeBackgroundTasks = {};
+
+YoutubeBackgroundTasks.getAllAvailableVideosFromYoutube = async () => {
+  try {
+    const res = await YoutubeApi.playlistItems.list({
+      part: 'snippet',
+      playlistId: process.env.YOUTUBE_UPLOAD_PLAYLIST_ID,
+    });
+    return res.data.items;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
 
 // Request every 10 mins
 YoutubeBackgroundTasks.auloUpdateYoutubeVideos = cron.schedule('*/10 * * * *', () => {
