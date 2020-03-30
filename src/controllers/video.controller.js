061e2a39407c4b8e637a0eaab7714cf64b5d3c9c
Bui Minh Chien <enpopo2101@gmail.com>
2020-03-30 16:15:18 +0700

    edit routes

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -4,7 +4,7 @@ const VideoController = {};
 
 VideoController.videoById = async (req, res) => {
   try {
-    const video = await VideoService.videoById(req.body.videoId);
+    const video = await VideoService.videoById(req.params.videoId);
     return res.status(200).json({ status: 200, data: video });
   } catch (error) {
     return res.status(400).json({ status: 400, message: error.message });
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -3,6 +3,6 @@ import VideoController from '../controllers/video.controller';
 
 const router = express.Router();
 
-router.get('/videoById', (req, res) => VideoController.videoById(req, res));
+router.get('/:videoId', (req, res) => VideoController.videoById(req, res));
 
 export default router;
